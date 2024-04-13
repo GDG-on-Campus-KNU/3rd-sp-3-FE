7c66c9b5f79cf02dd46e799ba20ecc3bcb85866f
junyeokk <cjh4302@gmail.com>
2024-04-13 21:41:51 +0900

    feat: yjs 라이브러리로 editor 동시 편집 세팅

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,21 +1,14 @@
 import * as Styles from "./styles";
-import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
-import "@blocknote/react/style.css";
+import { CollabSpace } from "@/widgets/CollabSpace";
 
 export const HomePage: React.FC = () => {
-  const editor = useCreateBlockNote();
-
   return (
     <>
       <Styles.HomePageLayout>
         <Styles.Channel>Channel Section</Styles.Channel>
         <Styles.Menu>Menu Section</Styles.Menu>
         <Styles.CollabSpace>
-          <Styles.BlockNoteWrapper>
-            <Styles.BlockNoteStyles />
-            <BlockNoteView editor={editor} />
-          </Styles.BlockNoteWrapper>
+          <CollabSpace />
         </Styles.CollabSpace>
         <Styles.ChatRoom>ChatRoom Section</Styles.ChatRoom>
       </Styles.HomePageLayout>
