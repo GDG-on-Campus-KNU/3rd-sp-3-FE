bfeb1d7236d02e5c8b621cc32630eb5a209fce61
junyeokk <cjh4302@gmail.com>
2024-04-24 16:58:10 +0900

    feat: dummy 데이터 추가 (#8)

--- src/features/channel/model/store.ts
+++ src/features/channel/model/store.ts
@@ -3,13 +3,59 @@ import { devtools } from "zustand/middleware";
 import { ChannelState } from "./types";
 
 const dummyChannels = [
-  { id: "1", name: "Channel 1" },
-  { id: "2", name: "Channel 2" },
-  { id: "3", name: "Channel 3" },
-  { id: "4", name: "Channel 4" },
-  { id: "5", name: "Channel 5" },
-  { id: "6", name: "Channel 6" },
-  { id: "7", name: "Channel 7" },
+  {
+    id: "1",
+    name: "프로젝트 채널 1",
+    description: "프로젝트 채널 1에 대한 소개입니다.",
+    members: [{ name: "사용자1" }, { name: "사용자2" }, { name: "사용자3" }],
+  },
+  {
+    id: "2",
+    name: "프로젝트 채널 2",
+    description: "프로젝트 채널 2에 대한 소개입니다.",
+    members: [{ name: "사용자1" }, { name: "사용자2" }],
+  },
+  {
+    id: "3",
+    name: "프로젝트 채널 3",
+    description: "프로젝트 채널 3에 대한 소개입니다.",
+    members: [{ name: "사용자1" }, { name: "사용자2" }, { name: "사용자3" }],
+  },
+  {
+    id: "4",
+    name: "프로젝트 채널 4",
+    description: "프로젝트 채널 4에 대한 소개입니다.",
+    members: [{ name: "사용자1" }],
+  },
+  {
+    id: "5",
+    name: "프로젝트 채널 5",
+    description: "프로젝트 채널 5에 대한 소개입니다.",
+    members: [{ name: "사용자1" }, { name: "사용자2" }],
+  },
+  {
+    id: "6",
+    name: "프로젝트 채널 6",
+    description: "프로젝트 채널 6에 대한 소개입니다.",
+    members: [
+      { name: "사용자1" },
+      { name: "사용자2" },
+      { name: "사용자3" },
+      { name: "사용자4" },
+    ],
+  },
+  {
+    id: "7",
+    name: "프로젝트 채널 7",
+    description: "프로젝트 채널 7에 대한 소개입니다.",
+    members: [
+      { name: "사용자1" },
+      { name: "사용자2" },
+      { name: "사용자3" },
+      { name: "사용자4" },
+      { name: "사용자5" },
+    ],
+  },
 ];
 
 const useStore = create<ChannelState>(
