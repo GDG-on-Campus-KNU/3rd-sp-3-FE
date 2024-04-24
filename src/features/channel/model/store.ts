485756c48f1d4869d3c0beb24adc80d472c67484
junyeokk <cjh4302@gmail.com>
2024-04-24 11:11:04 +0900

    feat: 채널 더미 데이터 추가

--- src/features/channel/model/store.ts
+++ src/features/channel/model/store.ts
@@ -2,11 +2,23 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { ChannelState } from "./types";
 
+const dummyChannels = [
+  { id: "1", name: "Channel 1" },
+  { id: "2", name: "Channel 2" },
+  { id: "3", name: "Channel 3" },
+  { id: "4", name: "Channel 4" },
+  { id: "5", name: "Channel 5" },
+  { id: "6", name: "Channel 6" },
+  { id: "7", name: "Channel 7" },
+];
+
 const useStore = create<ChannelState>(
   devtools<ChannelState>(
     (set) => ({
       currentChannel: null,
+      channels: dummyChannels,
       setCurrentChannel: (channel) => set({ currentChannel: channel }),
+      setChannels: (channels) => set({ channels }),
       sendMessage: (chatRoomId, message) =>
         set((state) => {
           const channel = state.currentChannel;
