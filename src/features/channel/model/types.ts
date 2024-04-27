7d7be208f385daccc871dd26da2c4424bc836623
junyeokk <cjh4302@gmail.com>
2024-04-27 18:35:42 +0900

    feat: Channel 내부 컴포넌트 타입 지정 (#8)

--- src/features/channel/model/types.ts
+++ src/features/channel/model/types.ts
@@ -1,23 +1,25 @@
-export interface ChatMessage {
-  senderId: string;
-  content: string;
-  timestamp: Date;
-}
-
-export interface ChatRoom {
-  id: string;
-  name: string;
-  messages: ChatMessage[];
-}
-
 export interface Channel {
   id: string;
   name: string;
   description?: string;
   members: Member[];
-  // documents: string[];
-  // chatRooms: ChatRoom[];
-  // voiceChatParticipants: string[];
+  documents: Document[];
+  chatRooms: ChatRoom[];
+  voiceRooms: VoiceRoom[];
+}
+
+export interface Item {
+  id: string;
+  name: string;
+  microphoneStatus?: "on" | "off";
+}
+
+export interface ItemListProps {
+  label: string;
+  items: Item[];
+  addItem?: () => void;
+  renderIconBefore?: (item: Item) => React.ReactNode;
+  renderIconAfter?: (item: Item) => React.ReactNode;
 }
 
 export interface ChannelState {
@@ -29,6 +31,29 @@ export interface ChannelState {
 }
 
 export interface Member {
+  id: string;
   name: string;
   microphoneStatus: "on" | "off";
+}
+
+export interface Document {
+  id: string;
+  title: string;
+}
+
+export interface ChatRoom {
+  id: string;
+  name: string;
+  messages: ChatMessage[];
+}
+
+export interface ChatMessage {
+  senderId: string;
+  content: string;
+  timestamp: Date;
+}
+
+export interface VoiceRoom {
+  id: string;
+  name: string;
 }
