4a6c81004d96969c08c67f97ce9b1bcb9e09c611
junyeokk <cjh4302@gmail.com>
2024-04-27 18:48:45 +0900

    feat: 음성채팅 목록 (VoiceroomList) 구현 (#8)

--- src/widgets/VoiceroomList/ui/VoiceroomList.tsx
+++ src/widgets/VoiceroomList/ui/VoiceroomList.tsx
@@ -1,3 +1,30 @@
+import useStore from "@/features/channel/model/store";
+import { ItemList } from "@/shared/ui/ItemList/ItemList";
+import VoiceIcon from "@/assets/voice.png";
+
+const renderVoiceRoomIcon = () => (
+  <img
+    src={VoiceIcon}
+    alt="Voice Room"
+    style={{ width: "24px", height: "24px", marginRight: "10px" }}
+  />
+);
+
 export const VoiceroomList: React.FC = () => {
-  return <></>;
+  const { currentChannel } = useStore((state) => ({
+    currentChannel: state.currentChannel,
+  }));
+
+  return (
+    <ItemList
+      label="음성"
+      items={
+        currentChannel?.voiceRooms.map((room) => ({
+          id: room.id,
+          name: room.name,
+        })) || []
+      }
+      renderIconBefore={renderVoiceRoomIcon}
+    />
+  );
 };
