e76d8780fdad35c039d6c4ea8ad45bff21a2691e
Dobbymin <gmin0701@knu.ac.kr>
2024-05-03 22:08:02 +0900

    feat: LoginPage Router 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "@/pages/home";
+import { createBrowserRouter } from 'react-router-dom';
+import { HomePage } from '@/pages/home';
+import { LoginPage } from '@/pages/login/ui';
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>,
-  }
+	{
+		path: '/',
+		element: <HomePage />,
+	},
+	{
+		path: 'login',
+		element: <LoginPage />,
+	},
 ]);
