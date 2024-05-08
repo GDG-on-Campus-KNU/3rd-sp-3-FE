4e901413f930a7f712251bd2873e9d072dd667d2
Dobbymin <gmin0701@knu.ac.kr>
2024-05-08 15:21:08 +0900

    feat: localhost https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
-})
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		mkcert({
+			certFileName: './localhost.pem',
+			keyFileName: './localhost-key.pem',
+		}),
+	],
+	// server: {
+	// 	https: true,
+	// },
+});
