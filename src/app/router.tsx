227f03aa45e93b798176d626c81a43557136bc51
Dobbymin <gmin0701@knu.ac.kr>
2024-05-08 17:15:28 +0900

    fix: 사용하지 않는 부분 주석 처리

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { HomePage } from '@/pages/home';
 // import { RegisterPage } from '@/pages/Register/ui';
-import { LoginPage } from '@/pages/login/ui/login';
+import { LoginPage } from '@/pages/login/ui';
 import { SocialLogin } from '@/pages/login/ui/socialLogin';
-import { Home } from '@/pages/login/ui/home';
 
 export const router = createBrowserRouter([
 	{
@@ -15,13 +14,12 @@ export const router = createBrowserRouter([
 		children: [
 			{
 				path: 'login',
-				// element: <LoginPage />,
-				element: <Home />,
+				element: <LoginPage />,
 			},
-			{
-				path: 'redirect',
-				element: <SocialLogin />,
-			},
+			// {
+			// 	path: 'redirect',
+			// 	element: <SocialLogin />,
+			// },
 		],
 	},
 ]);
--- src/pages/login/api/getEmail.tsx
+++ src/pages/login/api/getEmail.tsx
@@ -1,32 +1,32 @@
-import axios from "axios";
-import { ReactElement } from "react";
-import { useAuthStore } from "@/features/login/model/store";
-import { CustomBtn } from "@/widgets/login/ui";
+// import axios from "axios";
+// import { ReactElement } from "react";
+// import { useAuthStore } from "@/features/login/model/store";
+// import { CustomBtn } from "@/widgets/login/ui";
 
-export const GetEmail = (): ReactElement => {
-    const authToken = useAuthStore((state) => state.authToken);
+// export const GetEmail = (): ReactElement => {
+//     const authToken = useAuthStore((state) => state.authToken);
 
-    const CheckAuth = async (accessToken: string) => {
-        await axios
-        .get("https://www.googleapis.com/oauth2/v2/userinfo", {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-            }
-        )
-        .then((res) => {
-            alert(res.data.email);
-        })
-        .catch((err) => {
-            alert("oAuth token expired");
-            window.location.assign("http://localhost:5173");
-        });
-    };
-    return (
-        <CustomBtn onClick={()=> {
-            CheckAuth(authToken);
-        }}>
-            getEmail
-        </CustomBtn>
-    )
-};
+//     const CheckAuth = async (accessToken: string) => {
+//         await axios
+//         .get("https://www.googleapis.com/oauth2/v2/userinfo", {
+//             headers: {
+//                 Authorization: `Bearer ${accessToken}`,
+//             },
+//             }
+//         )
+//         .then((res) => {
+//             alert(res.data.email);
+//         })
+//         .catch((err) => {
+//             alert("oAuth token expired");
+//             window.location.assign("http://localhost:5173");
+//         });
+//     };
+//     return (
+//         <CustomBtn onClick={()=> {
+//             CheckAuth(authToken);
+//         }}>
+//             getEmail
+//         </CustomBtn>
+//     )
+// };
