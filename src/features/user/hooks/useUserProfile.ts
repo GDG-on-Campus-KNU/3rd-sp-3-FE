4e5d01c72ac28a6c0574849da63ba2cbfcfb392c
junyeokk <cjh4302@gmail.com>
2024-05-11 09:20:05 +0900

    feat: 유저 정보 훅 구현 (#10)

---
+++ src/features/user/hooks/useUserProfile.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { fetchUserProfile } from "@/features/api/apiService";
+import { useUserStore } from "@/features/user/model/store";
+
+export const useUserProfile = () => {
+  const currentUser = useUserStore((state) => state.currentUser);
+  const setUser = useUserStore((state) => state.setUser);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetchUserProfile();
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user data", error);
+
+        // API가 실제로 호출되지 않으므로 아래 코드는 주석 처리
+        // 실제로 API가 호출되면 주석을 해제하고 사용
+        // setCurrentUser(null);
+
+        // 테스트를 위해 하드코딩으로 사용자 데이터 설정
+        setUser({ id: "1", name: "Junhyeok Chae", email: "cjh4302@gmail.com" });
+      }
+    };
+
+    fetchUserData();
+  }, [setUser]);
+
+  return currentUser;
+};
