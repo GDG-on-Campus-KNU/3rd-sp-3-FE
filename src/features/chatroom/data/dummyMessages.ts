1f56e7011d2fd601aef592f95e5d963e40a83294
junyeokk <cjh4302@gmail.com>
2024-05-11 09:16:39 +0900

    feat: dummy 데이터 분리 (#10)

---
+++ src/features/chatroom/data/dummyMessages.ts
@@ -0,0 +1,124 @@
+export const dummyMessages = [
+  {
+    id: 1,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "준혁",
+    userId: "1",
+  },
+  {
+    id: 2,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    sender: "민준",
+    userId: "2",
+  },
+  {
+    id: 3,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    sender: "서연",
+    userId: "3",
+  },
+  {
+    id: 4,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "하준",
+    userId: "4",
+  },
+  {
+    id: 5,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "유진",
+    userId: "5",
+  },
+  {
+    id: 6,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+
+    sender: "준혁",
+    userId: "1",
+  },
+  {
+    id: 7,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "민준",
+    userId: "2",
+  },
+  {
+    id: 8,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+
+    sender: "서연",
+    userId: "3",
+  },
+  {
+    id: 9,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "하준",
+    userId: "4",
+  },
+  {
+    id: 10,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "유진",
+    userId: "5",
+  },
+  {
+    id: 11,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "준혁",
+    userId: "1",
+  },
+  {
+    id: 12,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "민준",
+    userId: "2",
+  },
+  {
+    id: 13,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "서연",
+    userId: "3",
+  },
+  {
+    id: 14,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "하준",
+    userId: "4",
+  },
+  {
+    id: 15,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "유진",
+    userId: "5",
+  },
+  {
+    id: 16,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "준혁",
+    userId: "1",
+  },
+  {
+    id: 17,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "민준",
+    userId: "2",
+  },
+  {
+    id: 18,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "서연",
+    userId: "3",
+  },
+  {
+    id: 19,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "준혁",
+    userId: "1",
+  },
+  {
+    id: 20,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    sender: "유진",
+    userId: "5",
+  },
+];
