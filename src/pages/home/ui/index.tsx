e309849ec603eb6715088e7f0ccedc77f0c363dd
Dobbymin <gmin0701@knu.ac.kr>
2024-05-12 15:53:09 +0900

    fix: 협업 공간 디자인 및 컴포넌트 수정

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,20 +1,27 @@
-import * as Styles from "./styles";
-import { ChannelWidget } from "@/widgets/ChannelSpace/ChannelWidget";
-import { CollabSpace } from "@/widgets/CollabSpace";
+import { DocumentNavigate } from '@/pages/CollabSpace/DocumentNavigate/ui';
+import { ChannelWidget } from '@/widgets/ChannelSpace/ChannelWidget';
+import * as Styles from './styles';
+import { DocumentSpace } from '@/pages/CollabSpace/DocumentSpace';
+import { CollabNavigate } from '@/pages/CollabNavigate/ui';
+import { useState } from 'react';
+// import { CollabSpace } from '@/widgets/Collabpace';
 
 export const HomePage: React.FC = () => {
-  return (
-    <>
-      <Styles.HomePageLayout>
-        <Styles.Channel>
-          <ChannelWidget />
-        </Styles.Channel>
-        <Styles.Menu>Menu Section</Styles.Menu>
-        <Styles.CollabSpace>
-          <CollabSpace />
-        </Styles.CollabSpace>
-        <Styles.ChatRoom>ChatRoom Section</Styles.ChatRoom>
-      </Styles.HomePageLayout>
-    </>
-  );
+	// const [isDocumentSpace, setIsDocumentSpace] = useState(true);
+
+	return (
+		<>
+			<Styles.HomePageLayout>
+				<Styles.Channel>
+					<ChannelWidget />
+				</Styles.Channel>
+				<Styles.Menu>Menu Section</Styles.Menu>
+				<Styles.CollabSpace>
+					{/* <CollabNavigate /> */}
+					<DocumentNavigate />
+				</Styles.CollabSpace>
+				<Styles.ChatRoom>ChatRoom Section</Styles.ChatRoom>
+			</Styles.HomePageLayout>
+		</>
+	);
 };
--- src/pages/home/ui/styles.ts
+++ src/pages/home/ui/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from 'styled-components';
 
 export const BlockNoteStyles = createGlobalStyle`
   .bn-container, .bn-editor {
@@ -11,51 +11,60 @@ export const BlockNoteStyles = createGlobalStyle`
 `;
 
 export const BlockNoteWrapper = styled.div`
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 `;
 
 export const Section = styled.section`
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  margin: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  text-align: left;
-  background: #f0f0f0;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+	border: 1px solid #ddd;
+	border-radius: 4px;
+	margin: 8px;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	text-align: left;
+	background: #f0f0f0;
+	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
 `;
 
 export const HomePageLayout = styled.div`
-  display: grid;
-  grid-template-areas:
-    "channel menu menu menu"
-    "channel collabSpace collabSpace chatRoom";
-  grid-template-columns: 350px minmax(auto, 1fr) 0px 350px;
-  grid-template-rows: auto 1fr;
-  gap: 8px;
-  height: 100vh;
-  padding: 8px;
-  box-sizing: border-box;
+	display: grid;
+	grid-template-areas:
+		'channel menu menu menu'
+		'channel collabSpace collabSpace chatRoom';
+	grid-template-columns: 350px minmax(auto, 1fr) 0px 350px;
+	grid-template-rows: auto 1fr;
+	gap: 8px;
+	height: 100vh;
+	padding: 8px;
+	box-sizing: border-box;
+`;
+
+export const CollabSpace = styled.section`
+	/* padding: 16px; */
+	border: 1px solid #ddd;
+	border-radius: 4px;
+	margin: 8px;
+	display: flex;
+	flex-direction: column;
+	/* justify-content: flex-start; */
+	/* align-items: flex-start; */
+	/* text-align: left; */
+	background: #f0f0f0;
+	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
 `;
 
 export const Channel = styled(Section)`
-  grid-area: channel;
+	grid-area: channel;
 `;
 
 export const Menu = styled(Section)`
-  grid-area: menu;
-  padding: 16px;
-`;
-
-export const CollabSpace = styled(Section)`
-  grid-area: collabSpace;
-  padding: 16px;
+	grid-area: menu;
+	padding: 16px;
 `;
 
 export const ChatRoom = styled(Section)`
-  grid-area: chatRoom;
-  padding: 16px;
+	grid-area: chatRoom;
+	padding: 16px;
 `;
