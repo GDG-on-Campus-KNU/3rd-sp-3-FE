916557a2506b142ba251d61aea110252cce2c52f
junyeokk <cjh4302@gmail.com>
2024-05-25 21:44:00 +0900

    feat: 채팅 섹션에 채팅방 참여 버튼 구현 (#10)

--- src/widgets/ChannelSpace/ChannelWidget/ui/ChatroomList.tsx
+++ src/widgets/ChannelSpace/ChannelWidget/ui/ChatroomList.tsx
@@ -1,11 +1,28 @@
+import React from "react";
 import useStore from "@/features/channel/model/store";
 import { ItemList } from "@/shared/ui/ItemList/ItemList";
 import { ChatBubbleIcon } from "@/shared/assets/icons/StyledIcon";
+import { enterChatRoom } from "@/features/chatroom/utils/stompClient";
+import { JoinButton } from "@/shared/styles/JoinButton";
 
 export const ChatroomList: React.FC = () => {
   const { currentChannel } = useStore((state) => ({
     currentChannel: state.currentChannel,
   }));
+  const userNickname = "준혁"; // 사용자 닉네임 설정
+
+  const handleRoomClick = () => {
+    console.log(
+      `Attempting to enter room in channel ${currentChannel?.id} as ${userNickname}`
+    );
+    if (currentChannel) {
+      enterChatRoom(currentChannel.id, userNickname);
+    }
+  };
+
+  const renderJoinButton = () => (
+    <JoinButton onClick={() => handleRoomClick()}>참여</JoinButton>
+  );
 
   return (
     <ItemList
@@ -17,6 +34,7 @@ export const ChatroomList: React.FC = () => {
         })) || []
       }
       renderIconBefore={() => <ChatBubbleIcon />}
+      renderIconAfter={() => renderJoinButton()}
     />
   );
 };
